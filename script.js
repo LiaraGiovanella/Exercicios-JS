64683224d191c3e3325eacef23273afef8f619b3
Liara Giovanella <giovanella.lia@gmail.com>
2023-10-11 07:50:12 -0300

    adicionando JS da fatoração

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//AJAX
+
 function carregarPagina(busca) {
     let localPagina = document.querySelector('.paginas')
 
@@ -12,6 +14,18 @@ function carregarPagina(busca) {
     requisicao.open('GET', `${busca}.html`)
     requisicao.send()
 }
+
+// Media
+
+function adicionar() {
+
+}
+
+function calcular() {
+
+}
+
+// Par ou Impar
 
 function parImpar() {
     var num = document.querySelector('#num')
@@ -30,5 +44,24 @@ function parImpar() {
         } else {
             res.innerHTML = `O número ${num} é <b>ímpar</b>!`
         }
+    }
+}
+
+// Fatoração
+
+function fatoracao() {
+    var num = document.querySelector('#num')
+    var res = document.querySelector('#res')
+    res.innerHTML = ''
+
+    if (num.value.length == 0) {
+        alert('Insira um valor para calcular!')
+    } else {
+        num = Number(num.value)
+        var mult = 1
+        for (var i = num; i > 1; i--) {
+            mult *= i
+        }
+        res.innerHTML = `O fatorial de ${num} é <b>${mult}</b>`
     }
 }
